3b86b9b3e264a9a5e23a8680fd8c1a1cc7105789
aliatayee <aliaqaatayee@gmail.com>
2022-10-13 16:41:40 +0430

    Add cancancan ability

---
+++ app/models/ability.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+  def initialize(user)
+    return unless user.present?
+    can :manage, Category, user_id: user.id
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,8 @@
 class User < ApplicationRecord
-    # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   has_many :activities,dependent: :destroy, foreign_key: 'author_id'
   has_many :categories, dependent: :destroy
   validates :name, presence: true, allow_blank: false, length: { maximum: 250 }
-  # after_initialize :set_defaults
 
 end
