34480c3fc4cb5f71a7f5afabac3afc00ae716d2d
aliatayee <aliaqaatayee@gmail.com>
2022-10-14 23:15:27 +0430

    Update models

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,9 +1,8 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
   def initialize(user)
     return unless user.present?
+
     can :manage, Category, user_id: user.id
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :activities,dependent: :destroy, foreign_key: 'author_id'
+  has_many :activities, dependent: :destroy, foreign_key: 'author_id'
   has_many :categories, dependent: :destroy
   validates :name, presence: true, allow_blank: false, length: { maximum: 250 }
-
 end
